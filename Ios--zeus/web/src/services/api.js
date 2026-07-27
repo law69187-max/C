48b6e6144a484b9949cacee84bfced14e274c9d1
law69187-max <law69187@gmail.com>
2026-07-27 03:40:04 +0300

    Update api.js

--- Ios--zeus/web/src/services/api.js
+++ Ios--zeus/web/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'https://c-production-e6c3.up.railway.app'; 
+const API_URL = 'https://web-production-0616b.up.railway.app'; 
 
 const api = axios.create({
   baseURL: API_URL,
